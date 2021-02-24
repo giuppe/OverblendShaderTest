b61f0fd4b6bffa0e5b8d206549eb68efb88e03e4
giuppe <giuseppe.da@gmail.com>
2021-02-24 18:01:11 +0100

    little polishing and comments

--- source/OverblendShader.hx
+++ source/OverblendShader.hx
@@ -7,7 +7,6 @@ class OverblendShader extends FlxGraphicsShader
 
         uniform vec2 sunPosition;
         uniform float sunRadius;
-        
         uniform float power;
 
         void main()
@@ -21,25 +20,27 @@ class OverblendShader extends FlxGraphicsShader
 
             vec4 result = source;
             if(power==1.0){
-            vec3 lightColor = vec3(1.0, 0.9, 0.25);
+                vec3 lightColor = vec3(1.0, 0.9, 0.25);
 
-            float intensity = 0.8; //1.0-sunPosition.y;
-            float radius = 0.9; //1.0-sunPosition.y;
+                float intensity = 0.8; 
+                float radius = 0.9; 
 
-            vec2 pos = pixpos-sunPosition;
-            float dist = 1./length(pos);
+                vec2 pos = pixpos-sunPosition;
+                float dist = 1./length(pos);
             
-            dist *= radius;
+                dist *= radius;
     
 
-            dist = pow(dist, intensity);
+                dist = pow(dist, intensity);
 
-            vec3 col = dist * lightColor;
+                vec3 col = dist * lightColor;
             
-            //exponential decrease to limit the filter
-            //col = 1.0 - exp( -col );
+                //exponential decrease to limit the filter
+                //useful to clamp rgb components to remove artefacts 
+                //if the light color is not white-ish
+                //col = 1.0 - exp( -col );
             
-            result = vec4(source.rgb*col, 1.0);
+                result = vec4(source.rgb*col, 1.0);
             }
             
             gl_FragColor = result;
